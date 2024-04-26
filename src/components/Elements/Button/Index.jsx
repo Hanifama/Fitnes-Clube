32786539ce6526daea926c1b72224b670407d5d0
hanifama <hanifamafisial.com>
2024-04-26 18:38:19 +0700

    fix bug navbar

--- src/components/Elements/Button/Index.jsx
+++ src/components/Elements/Button/Index.jsx
@@ -1,12 +1,15 @@
 import { Children } from "react"
+import { Link } from "react-router-dom";
 
 const Button = (props) =>{
-    const {children, variant = "bg-black"}= props;
+    const {children, variant = "bg-black", toLink}= props;
     return(
         <button
         className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
         >
+        <Link to={toLink} className="font-bold text-white">
         {children}
+        </Link>        
         </button>
     )
 }
--- src/components/Elements/Navbar/Index.jsx
+++ src/components/Elements/Navbar/Index.jsx
@@ -1,40 +1,66 @@
 import Button from "../Button/Index"
 
-const Navbar = () => {
+const Navbar = (props) => {    
+    const navLinks = 
+[
+    {
+        id : 1,
+        title : 'Home',
+        path : 'header'
+    },
+    {
+        id : 2,
+        title : 'About',
+        path : 'Program'
+    },
+    {
+        id : 3,
+        title : 'Service',
+        path : 'service'
+    },
+    {
+        id : 4,
+        title : 'Program',
+        path : 'Program'
+    },
+    {
+        id : 5,
+        title : 'Community',
+        path : 'Community'
+    }
+
+]  
     return(
+        <div className="w-full h-screen bg-primary-light">
         <nav class="bg-primary-light mx-auto px-8 py-8 flex items-center justify-between gap-8 max-h-20">
          <img src="../public/img/logo-fit.png" alt="Logo" class="w-[150px] ml-11"/>
             <ul class="flex items-center gap-12 list-none">
-                <li class="link">
-                <a href="#" class="relative text-white pb-1 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5">Beranda</a>
-                </li>
-                <li class="link">
-                <a href="#" 
-                class="relative text-white pb-1 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5">Tentang</a>
-                </li>
-                <li class="link">
-                <a href="#" class="relative text-white pb-2 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5">Service</a>
-                </li> 
-                <li class="link">
-                <a href="#" class="relative text-white pb-2 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5">Program</a>
-                </li> 
-                <li class="link">
-                <a href="#" class="relative text-white pb-1 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5">Komunitas</a>
-                </li>            
+                {navLinks.map ((item) => 
+                    <li 
+                    class="relative text-white pb-3 hover:text-secondary-dark hover:after:w-8 hover:after:bg-secondary-dark hover:after:bottom-0 hover:after:left-0 hover:after:absolute hover:after:h-0.5"
+                    key={item.id}>
+                        <a href={item.path}>
+                            {item.title}
+                        </a>
+                    </li>
+                )}                         
             </ul>
-            <div className="">
+            <div>
                 <Button
-                variant= "bg-secondary-dark" 
+                variant= "bg-secondary-dark"
+                toLink = "/register"  
                 >
                 Sign Up
                 </Button>
                 <Button
                 variant= "tranparent" 
+                toLink = "/login"                
                 >
                 Sign in
                 </Button>
             </div>
         </nav>
+        </div>
 
       
     )
